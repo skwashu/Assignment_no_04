8182e9949858ed49a7119b7db16ada240ba34176
Shaikh Waseem <126464944+skwashu@users.noreply.github.com>
2023-03-25 13:15:35 +0530

    Add files via upload

---
+++ Play_with_lambda.py
@@ -0,0 +1,13 @@
+#Write a Python program to create a lambda function that adds 25 to a given number passed in as an argument.
+
+
+
+# sample input: 10
+
+
+r = lambda a : a + 25
+print(r(10))
+
+# Output
+
+# sample output: 35
