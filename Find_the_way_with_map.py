94c854d63594d054ab995cac5c07730ad4f0db7a
Shaikh Waseem <126464944+skwashu@users.noreply.github.com>
2023-03-25 13:14:42 +0530

    Add files via upload

---
+++ Find_the_way_with_map.py
@@ -0,0 +1,18 @@
+#Write a Python program to triple all numbers of a given list of integers. Use Python map.
+
+#sample list: [1, 2, 3, 4, 5, 6, 7]
+
+
+
+nums = (1, 2, 3, 4, 5, 6, 7) 
+print("Original list: ", nums)
+result = map(lambda x: x + x + x, nums) 
+print("\nTriple of said list numbers:")
+print(list(result))
+
+
+#0utput:
+# Original list:  (1, 2, 3, 4, 5, 6, 7)
+
+# Triple of said list numbers:
+# [3, 6, 9, 12, 15, 18, 21]
